c451f62631708b323d13afdae86e8d7da0a983a5
Anshul Kushwaha <anshulkushwaha1@gmail.com>
2018-04-03 13:31:43 +0530

    Index Snapshots

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Features
 - [x] Price details by symbol
 - [x] LastPrice by symbol
 - [x] Indices list
+- [x] Index Snapshot for all indices
 - [ ] Top Gainers
 - [ ] Top Losers
-- [ ] ???  
+- [ ] ???
--- index.js
+++ index.js
@@ -15,6 +15,54 @@ let urls = {
     indices: 'http://www.nseindia.com/homepage/Indices1.json',
 };
 
+let index_map = {
+    'NIFTY 50': 'nifty',
+    'NIFTY NEXT 50': 'juniorNifty',
+    'INDIA VIX': 'indiavix',
+    'CNX 100': 'cnx100',
+    'S&P CNX DEFTY': 'defty',
+    'S&P CNX 500': 'cnx500',
+    'CNX MIDCAP': 'midcap',
+    'NIFTY100 LIQUID 15': 'nseliquid',
+    'NIFTY MIDCAP 50': 'niftyMidcap50',
+    'NIFTY MIDCAP LIQUID 15': 'niftyMidcapLiq15',
+    'NIFTY AUTO': 'cnxAuto',
+    'NIFTY BANK': 'bankNifty',
+    'NIFTY ENERGY': 'cnxEnergy',
+    'NIFTY FINANCIAL SERVICES': 'cnxFinance',
+    'NIFTY FMCG': 'cnxFMCG',
+    'NIFTY IT': 'cnxit',
+    'NIFTY MEDIA': 'cnxMedia',
+    'NIFTY METAL': 'cnxMetal',
+    'NIFTY PHARMA': 'cnxPharma',
+    'NIFTY PSU BANK': 'cnxPSU',
+    'NIFTY REALTY': 'cnxRealty',
+    'NIFTY PRIVATE BANK': 'niftyPvtBank',
+    'NIFTY COMMODITIES': 'cnxCommodities',
+    'NIFTY INDIA CONSUMPTION': 'cnxConsumption',
+    'NIFTY CPSE': 'cpse',
+    'NIFTY INFRASTRUCTURE': 'cnxInfra',
+    'NIFTY MNC': 'cnxMNC',
+    'NIFTY GROWTH SECTOR 15': 'ni15',
+    'NIFTY PSE': 'cnxPSE',
+    'NIFTY SERVICES SECTOR': 'cnxService',
+    'NIFTY DIVIDEND OPPORTUNITIES 50': 'cnxDividendOppt',
+    'NIFTY50 VALUE 20': 'nv20',
+    'NIFTY QUALITY 30': 'niftyQuality30',
+    'NIFTY50 EQUAL WEIGHT': 'nifty50EqualWeight',
+    'NIFTY100 EQUAL WEIGHT': 'nifty100EqualWeight',
+    'NIFTY100 LOW VOLATILITY 30': 'nifty100LowVol30',
+    'NIFTY ALPHA 50': 'niftyAlpha50',
+    'SOVEREIGN GOLD BONDS': 'sovGold',
+    'FO STOCKS': 'foSec',
+    'ETF': 'etf',
+    'GOLD ETF': 'goldEtf',
+    'NIFTY 50 ETF': 'nifty50Etf',
+    'INSTITUTIONAL STOCK WATCH': 'iL',
+    'BL STOCK WATCH': 'bL',
+    'BONDS IN CM': 'cbmSecList',
+};
+
 let price = async (symbol) => {
     let url = urls.symbolPrice + '?symbol=' + symbol;
     let response = await got(url, got_options);
@@ -35,8 +83,20 @@ let indices = async () => {
     return response.data;
 };
 
+let indexSnapshot = async (index) => {
+    index = index.toUpperCase();
+    let url = 'https://www.nseindia.com/live_market/dynaContent/live_watch/stock_watch/' + index_map[index] + 'StockWatch.json';
+    // console.log(url);
+
+    let response = await got(url, got_options);
+    response = JSON.parse(response.body);
+    // console.log(response);
+    return response;
+};
+
 module.exports = {
     price,
     lastPrice,
-    indices
+    indices,
+    indexSnapshot
 };
